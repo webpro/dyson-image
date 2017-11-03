c86dcd07533e15256ed71e63e7f78498808475af
Lars Kappert <lars@webpro.nl>
2017-11-03 09:19:48 +0100

    Format + native Promise

--- index.js
+++ index.js
@@ -1,84 +1,79 @@
-var http = require('http'),
-    request = require('request'),
-    when = require('when'),
-    mimeMagic = require('node-ee-mime-magic');
+var request = require('request');
+var mimeMagic = require('node-ee-mime-magic');
 
 var isTest = function() {
-    return process.env.NODE_ENV === 'test';
+  return process.env.NODE_ENV === 'test';
 };
 
 var log = function() {
-    if(!isTest()) {
-        console.log.apply(console, arguments)
-    }
+  if (!isTest()) {
+    console.log.apply(console, arguments);
+  }
 };
 
 var imageCache = {};
 
 var imageRequest = function(options) {
+  options = options || {};
 
-    options = options || {};
+  var host = options.host || 'http://dummyimage.com';
+  var uri = host;
 
-    var host = options.host || 'http://dummyimage.com',
-        uri = host,
-        deferred = when.defer();
+  if (options.path) {
+    uri += options.path;
+  } else if (options.width && options.height) {
+    uri += '/' + options.width + 'x' + options.height;
+  } else if (options.width) {
+    uri += '/' + options.width;
+  }
 
-    if(options.path) {
-        uri += options.path;
-    } else if(options.width && options.height) {
-        uri += '/' + options.width + 'x' + options.height;
-    } else if(options.width) {
-        uri += '/' + options.width;
-    }
-
-    request({
+  return new Promise((resolve, reject) => {
+    request(
+      {
         uri: uri,
         encoding: 'binary'
-    }, function(error, response, body) {
-
-        if(error) {
-            deferred.reject(error);
+      },
+      function(error, response, body) {
+        if (error) {
+          return reject(error);
         }
 
-        if(!error && response.statusCode === 200) {
+        if (!error && response.statusCode === 200) {
+          var imageBuffer = new Buffer(body, 'binary');
 
-            var imageBuffer = new Buffer(body, 'binary');
+          mimeMagic(imageBuffer, function(error, result) {
+            if (error) {
+              return reject(error);
+            }
 
-            mimeMagic(imageBuffer, function(error, result) {
-
-                if(error) {
-                    deferred.reject(error);
-                }
-
-                deferred.resolve({
-                    mimeType: result.mime,
-                    buffer: imageBuffer
-                });
+            resolve({
+              mimeType: result.mime,
+              buffer: imageBuffer
             });
+          });
         }
-    });
-
-    return deferred.promise;
+      }
+    );
+  });
 };
 
 var asMiddleware = function(req, res, next) {
+  var path = req.url.replace('/image', '');
 
-    var path = req.url.replace('/image', '');
+  log('[dyson-image] Resolving response for', req.url, imageCache[path] ? '(cached)' : '');
 
-    log('[dyson-image] Resolving response for', req.url, imageCache[path] ? '(cached)' : '');
+  if (!imageCache[path]) {
+    imageCache[path] = imageRequest({ path: path });
+  }
 
-    if(!imageCache[path]) {
-        imageCache[path] = imageRequest({path: path});
-    }
-
-    imageCache[path].then(function(image) {
-        res.setHeader('Content-Type', image.mimeType);
-        res.write(image.buffer);
-        res.send();
-    });
+  imageCache[path].then(function(image) {
+    res.setHeader('Content-Type', image.mimeType);
+    res.write(image.buffer);
+    res.send();
+  });
 };
 
 module.exports = {
-    request: imageRequest,
-    asMiddleware: asMiddleware
+  request: imageRequest,
+  asMiddleware: asMiddleware
 };
